3686842f50571d5f38de199e5dd463f66f9847cf
abdihoucem <35077338+abdihoucem@users.noreply.github.com>
2022-04-24 15:21:27 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,12 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.11.0"
+      version = "~> 4.11.0"
     }
   }
-}
-
-
-terraform {
-  required_version = ">= 1.1.6"
 }
